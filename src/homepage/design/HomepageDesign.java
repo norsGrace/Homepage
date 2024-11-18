acbdcaa5fba32c73c3c90108052519026ec89159
norsGrace <noreengonzales86@gmail.com>
2024-11-18 14:23:30 +0800

    first commit

---
+++ src/homepage/design/HomepageDesign.java
@@ -0,0 +1,19 @@
+package homepage.design;
+
+/**
+ *
+ * @author Noreen Grace G
+ */
+public class HomepageDesign {
+
+    
+    public static void main(String[] args) {
+       //HOMEPAGE DESIGN//
+        
+         Homepage HomeFrame = new Homepage();
+         HomeFrame.setVisible(true);
+         HomeFrame.pack();
+         HomeFrame.setLocationRelativeTo(null);
+    }
+    
+}
